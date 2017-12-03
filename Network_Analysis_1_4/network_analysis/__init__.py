c336c13456eba749b8b2707fa359daaeb68d053a
Lhomme Serge <serge.lhomme@u-pec.fr>
2017-12-03 16:34:57 +0100

    Add files via upload

---
+++ Network_Analysis_1_4/network_analysis/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#-----------------------------------------------------------
+#
+# NetworkAnalysis
+# Copyright Serge Lhomme
+# EMAIL: serge.lhomme (at) u-pec.fr
+# WEB  : http://serge.lhomme.pagesperso-orange.fr/deven.html
+#
+# Tools for analyzing networks characteristics
+#
+#-----------------------------------------------------------
+#
+# licensed under the terms of GNU GPL 2
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+#---------------------------------------------------------------------
+
+def classFactory(iface):
+  from network_analysis import NetworkAnalysis
+  return NetworkAnalysis(iface)
