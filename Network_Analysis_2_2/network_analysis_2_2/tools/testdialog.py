fb4bcf60160c3f40d318371b0095ea961f7cbd17
Lhomme Serge <serge.lhomme@u-pec.fr>
2025-10-23 15:19:39 +0200

    Create testdialog.py

---
+++ Network_Analysis_2_2/network_analysis_2_2/tools/testdialog.py
@@ -0,0 +1,7 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from ui_test import Ui_Dialog
+class testDialog(QtWidgets.QDialog):
+  def __init__(self, parent):
+    QtWidgets.QDialog.__init__(self, parent)
+    self.ui = Ui_Dialog()
+    self.ui.setupUi(self)
